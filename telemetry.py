5da60d82d94ddb0fb8f3dd58cda18adeac8ca773
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2020-06-18 23:24:02 +0200

    add python scripts

---
+++ telemetry.py
@@ -0,0 +1,108 @@
+import requests
+import socket
+
+IP_ADDRESS     = '127.0.0.1' #socket.gethostbyname(socket.gethostname())
+URL_INDICATORS = 'http://{}:8111/indicators'.format(IP_ADDRESS)
+URL_STATE      = 'http://{}:8111/state'.format(IP_ADDRESS)
+URL_COMMENTS   = 'http://{}:8111/gamechat?lastId={}'
+URL_EVENTS     = 'http://{}:8111/hudmsg?lastEvt=-1&lastDmg={}'
+URL_MAP_IMG    = 'http://{}:8111/map.img'.format(IP_ADDRESS)
+URL_MAP_OBJ    = 'http://{}:8111/map_obj.json'.format(IP_ADDRESS)
+URL_MAP_INFO   = 'http://{}:8111/map_info.json'.format(IP_ADDRESS)
+FT_TO_M        = 0.3048
+IN_FLIGHT      = 0
+IN_MENU        = -1
+NO_MISSION     = -2
+WT_NOT_RUNNING = -3
+OTHER_ERROR    = -4
+METRICS_PLANES = ['p-', 'f-', 'f2', 'f3', 'f4', 'f6', 'f7', 'f8', 'f9', 'os',
+                  'sb', 'tb', 'a-', 'pb', 'am', 'ad', 'fj', 'b-', 'xp', 'bt',
+                  'xa', 'xf', 'sp', 'hu', 'ty', 'fi', 'gl', 'ni', 'fu', 'fu',
+                  'se', 'bl', 'be', 'su', 'te', 'st', 'mo', 'we', 'ha']
+
+_s = requests.Session()
+
+def find_altitude(indicators):
+    name = indicators['type']
+
+    # account for freedom units in US and UK planes
+    if name[:2] in METRICS_PLANES:
+        if 'altitude_10k' in indicators.keys():
+            return indicators['altitude_10k'] * FT_TO_M
+        elif 'altitude_hour' in indicators.keys():
+            return indicators['altitude_hour'] * FT_TO_M
+        elif 'altitude_min' in indicators.keys():
+            return indicators['altitude_min'] * FT_TO_M
+        else:
+            return 0
+    else:
+        if 'altitude_10k' in indicators.keys():
+            return indicators['altitude_10k']
+        elif 'altitude_hour' in indicators.keys():
+            return indicators['altitude_hour']
+        elif 'altitude_min' in indicators.keys():
+            return indicators['altitude_min']
+        else:
+            return 0
+
+def get_indicators():
+    try:
+        obj = _s.get(URL_INDICATORS, timeout=0.1).json()
+
+        if obj['valid']:
+            obj['aviahorizon_pitch'] = -obj['aviahorizon_pitch'] if 'aviahorizon_pitch' in obj else None
+            obj['aviahorizon_roll'] = -obj['aviahorizon_roll'] if 'aviahorizon_roll' in obj else None
+
+            obj['alt_m'] = find_altitude(obj)
+
+            return obj
+    except:
+        return None
+
+def get_state():
+    try:
+        obj = _s.get(URL_STATE, timeout=0.1).json()
+
+        if obj['valid']:
+            return obj
+    except:
+        return None
+
+def get_map_obj():
+    try:
+        obj = _s.get(URL_MAP_OBJ, timeout=0.1).json()
+
+        if obj['valid']:
+            return obj
+    except:
+        return None
+
+def get_map_info():
+    try:
+        obj = _s.get(URL_MAP_INFO, timeout=0.1).json()
+
+        if obj['valid']:
+            return obj
+    except:
+        return None
+
+def get_flight_data():
+    try:
+        ind = get_indicators()
+        state = get_state()
+
+        if ind['valid'] and state['valid']:
+            obj = {}
+
+            obj.update(ind)
+            obj.update(state)
+
+            return obj
+    except:
+        return None
+
+if __name__ == "__main__":
+    while True:
+        ind = get_indicators()
+        state = get_state()
+        pass
---
+++ wthud.py
@@ -0,0 +1,149 @@
+import tkinter as tk
+from tkinter import ttk
+import win32api, win32con, pywintypes
+from telemetry import get_flight_data
+import json
+
+class SettingsScreen(object):
+    def __init__(self, master):
+        self.master = master
+        self.container = ttk.Frame(self.master)
+        self.canvas = tk.Canvas(self.container)
+        self.scrollbar = ttk.Scrollbar(self.container, orient="vertical", command=self.canvas.yview)
+
+        self.btn_frame = ttk.Frame(self.master)
+        self.reload_btn = ttk.Button(self.btn_frame, text='Reload', command=self.reload)
+        self.save_btn = ttk.Button(self.btn_frame, text='Save Config', command=self.save)
+        self.load_btn = ttk.Button(self.btn_frame, text='Load Config', command=self.load)
+
+        self.scrollable_frame = ttk.Frame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas_elements = {}
+        self.craft_name = ''
+
+        self.container.pack(fill="both", expand=True)
+        self.btn_frame.pack()
+        self.reload_btn.pack(side="left")
+        self.save_btn.pack(side="left")
+        self.load_btn.pack(side="left")
+
+
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
+
+    def save(self):
+        if self.craft_name:
+            options = {}
+            for text, (enable, disp, unit, _, _, _) in self.canvas_elements.items():
+                if enable.get():
+                    options[text] = (disp.get(), unit.get())
+            with open(f'{self.craft_name}_hud.json', 'w') as outf:
+                json.dump(options, outf)
+        pass
+
+    def load(self):
+        self.reload()
+        if self.craft_name:
+            with open(f'{self.craft_name}_hud.json', 'r') as inf:
+                options = json.load(inf)
+
+                for text, (disp, unit) in options.items():
+                    self.canvas_elements[text][0].set(1)
+                    self.canvas_elements[text][1].set(disp)
+                    self.canvas_elements[text][2].set(unit)
+            pass
+
+    def add_to_canvas(self, text):
+        enable = tk.IntVar()
+        disp = tk.StringVar()
+        unit = tk.StringVar()
+        disp.set(text)
+        unit.set('')
+
+        cb = ttk.Checkbutton(self.scrollable_frame, text=text, variable=enable)
+        e = ttk.Entry(self.scrollable_frame, width=20, textvariable=disp)
+        e2 = ttk.Entry(self.scrollable_frame, width=20, textvariable=unit)
+
+        cb.grid(column=0, row=len(self.canvas_elements), sticky=tk.W)
+        e.grid(column=1, row=len(self.canvas_elements))
+        e2.grid(column=2, row=len(self.canvas_elements))
+
+        #self.canvas_elements.append((enable, disp, unit, text, cb, e, e2))
+        self.canvas_elements[text] = (enable, disp, unit, cb, e, e2)
+
+    def destroy_canvas(self):
+        for _, (_, _, _, cb, e, e2) in self.canvas_elements.items():
+            cb.destroy()
+            e.destroy()
+            e2.destroy()
+
+        self.canvas_elements.clear()
+
+    def reload(self):
+        self.destroy_canvas()
+        obj = get_flight_data()
+
+        if obj:
+            self.craft_name = obj['type']
+            for k, v in obj.items():
+                self.add_to_canvas(k)
+
+class HUDScreen(object):
+    def __init__(self, master, canvas_elements):
+        self.master = master
+        self.canvas_elements = canvas_elements
+        self.toplevel = tk.Toplevel(self.master)
+        self.label = tk.Label(self.toplevel, text='Climb Rate', font=('Courier New', '14', 'bold'), fg='white', bg='black', justify=tk.LEFT)
+        self.toplevel.overrideredirect(True)
+        self.toplevel.geometry("+25+500")
+        self.toplevel.lift()
+        self.toplevel.wm_attributes("-topmost", True)
+        self.toplevel.wm_attributes("-disabled", True)
+        self.toplevel.wm_attributes("-transparentcolor", "black")
+
+        hWindow = pywintypes.HANDLE(int(self.toplevel.frame(), 16))
+        # http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+        # The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
+        exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+        win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+
+        self.label.pack()
+
+    def update(self):
+        rate = 1000
+        label_text = ''
+
+        obj = get_flight_data()
+        try:
+            if obj:
+                rate = 100
+                for var_name, (on, disp, unit, _, _, _) in self.canvas_elements.items():
+                    if on.get():
+                        label_text += f'{disp.get():<10}{obj[var_name]:7.1f} {unit.get()}\n'
+        except:
+            pass
+
+        self.label.config(text=label_text)
+
+        self.master.after(rate, self.update)
+
+
+root = tk.Tk()
+
+app = SettingsScreen(root)
+hud = HUDScreen(root, app.canvas_elements)
+hud.update()
+
+root.mainloop()
