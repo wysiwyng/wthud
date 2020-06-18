3dd32d2716f40ca2a1d53159cd1a0900fec4b3b7
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2020-06-19 01:14:16 +0200

    add license

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # wthud
 Head-up Display for additional War Thunder data. Written in Python 3, uses data exposed by War Thunder on ```localhost:8111```. The shown data is configurable per aircraft.
 
+Compatible only with Windows! Transparency is handled by the window manager / compositor in most Linux distros, and therefore neither easy nor portable to handle.
+
 ## Usage
 1. Download source code
 2. No external libraries needed, just run ```python wthud.py```
@@ -9,3 +11,6 @@ Head-up Display for additional War Thunder data. Written in Python 3, uses data 
 5. Chose the variables that should be shown in the HUD by checking them. Give names and units in the two checkboxes
 6. Save your config with the ```Save``` button.
 7. When entering a new game, load a saved config with the ```Load``` button
+
+## License
+This program is licensed under a GNU GPL v3 license, see [COPYING] for details.
--- wthud.py
+++ wthud.py
@@ -1,5 +1,24 @@
+# wthud - Head-up Display for War Thunder
+# Copyright (C) 2020 wysiwyng
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Main graphics functions
+
 import tkinter as tk
 from tkinter import ttk
+
 import win32api, win32con, pywintypes
 from telemetry import get_flight_data
 import json
